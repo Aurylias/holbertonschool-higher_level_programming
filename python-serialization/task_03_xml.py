72a211215c998d1096727472e0df49f7fa0e97e1
Aurylias <brycevermorel@gmail.com>
2026-09-22 10:11:59 +0200

    Initial commit

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,20 @@
+"""Module to serialize and deserialize using XML"""
+import xml.etree.ElementTree as ET
+
+def serialize_to_xml(dictionary, filename):
+    """Serialize a Python dictionary into XML and save it to the given file"""
+    root = ET.Element("data")
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key)
+        child.text = value
+    tree = ET.ElementTree()
+    tree.write(filename)
+
+def deserialize_from_xml(filename):
+    """Deserialize an XML file into a Python dictionary"""
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    data = {}
+    for child in root:
+        data[child.tag] = child.text
+    return data
