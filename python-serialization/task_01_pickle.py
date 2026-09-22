32f5bd5a00cee3ee63bd3a3779bad40ef9c1e011
Aurylias <brycevermorel@gmail.com>
2026-09-22 09:37:30 +0200

    docs: add docstring

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -2,6 +2,7 @@
 from pickle import dump, load
 
 class CustomObject:
+    """Constructor"""
     def __init__(self, name="", age=0, is_student=False):
         self.name = name
         self.age = age
@@ -9,35 +10,42 @@ class CustomObject:
 
     @property
     def name(self):
+        """Getter for name"""
         return self.__age
 
     @name.setter
     def name(self, name):
+        """Setter for name"""
         if not isinstance(name, str):
             raise TypeError("name must be a string")
         self.__name = name
 
     @property
     def age(self):
+        """Getter for age"""
         return self.__age
 
     @age.setter
     def age(self, age):
+        """Setter for age"""
         if not isinstance(age, int):
             raise TypeError("age must be an int")
         self.__age = age
 
     @property
     def is_student(self):
+        """Getter for is_student"""
         return self.__is_student
 
     @is_student.setter
     def is_student(self, is_student):
+        """Setter for is_student"""
         if not isinstance(is_student, bool):
             raise TypeError("is_student must be a bool")
 
 
     def display(self):
+        """Display all attributes"""
         print(f"Name: {self.name}\nAge: {self.age}\nIs student: \
               {self.is_student}")
 
