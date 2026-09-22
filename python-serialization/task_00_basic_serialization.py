caa9b1034df3f5f6166470c9d3194f9fde3de6d5
Aurylias <brycevermorel@gmail.com>
2026-09-22 08:49:49 +0200

    fix: change open mode in first function

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -3,7 +3,7 @@ from json import dump, load
 """Module to serialize/deserialize a Python dictionary in JSON"""
 def serialize_and_save_to_file(data, filename):
     """Serialize a JSON Python dictionary"""
-    with open(filename, "x") as jsonFile:
+    with open(filename, "w") as jsonFile:
         json.dump(data, jsonFile)
 
 def load_and_deserialize(filename):
